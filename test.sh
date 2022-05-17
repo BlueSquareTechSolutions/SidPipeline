ed1582281246e601c7bee2184449b71a51497de5
SiddhivinayakKudale <88762403+SiddhivinayakKudale@users.noreply.github.com>
2022-05-18 09:57:07 +1000

    added populate db commands

--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 echo "Hello World"
 foo=$(kubectl get pods | awk '{print $1}' | grep -e "api")
 kubectl exec -it {foo} -- /bin/sh
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py populatedb
 echo "Over and out."
